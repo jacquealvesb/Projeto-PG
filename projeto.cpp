9fece15f25a95a365ce6b500697c2cf5bd275dd0
Jacqueline Alves Barbosa <jab3@ug1c21.windows.cin.ufpe.br>
2019-05-06 17:15:44 -0300

    Materiais difusos

--- projeto.cpp
+++ projeto.cpp
@@ -1,54 +1,68 @@
 #include <iostream>
-#include "ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
+#include "float.h"
+#include "camera.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r){
-	vec3 oc = r.origin() - center;
-  	float a = dot(r.direction(), r.direction());
-  	float b = 2.0 * dot(oc, r.direction());
-  	float c = dot(oc, oc) - radius*radius;
-  	float discriminant = b*b - 4*a*c;
-  	
-  	if(discriminant < 0)
-  		return -1.0;
-  	else
-      return (-b - sqrt(discriminant)) / (2.0*a);
+vec3 random_in_unit_sphere() {
+    vec3 p;
+
+    do {
+        p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while(p.squared_length() >= 1.0);
+
+    return p;
 }
 
-vec3 color(const ray& r){
-  
-  	float t = hit_sphere(vec3(0,0,-1), 0.5, r);
-  
-  	if(t>0.0){
-      vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
-      return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+vec3 color(const ray& r, hitable *world){
+    hit_record rec;
+
+  	if(world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere(); 
+        
+        return 0.5*color(ray(rec.p, target-rec.p), world);
+
+    } else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5*(unit_direction.y() + 1.0);
+        
+        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
-  
-  	vec3 unit_direction = unit_vector(r.direction());
-  
-  	t = 0.5*(unit_direction.y() + 1.0);
-  	return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
 }
-
 
 int main() {
     int nx = 400;
     int ny = 200;
-    
+    int ns = 100;
+
+    int cameraSizeX = 4.0;
+    int cameraSizeY = 2.0;
+
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    vec3 lower_left_corner(-4.0, -2.0, -1.0);
-    vec3 horizontal(8.0, 0.0, 0.0);
-    vec3 vertical(0.0, 4.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
+    hitable *list[2];
+
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    
+    hitable *world = new hitable_list(list, 2);
+
+    camera cam;
 
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
+            vec3 col(0, 0, 0);
+            for(int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
 
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
 
-            vec3 col = color(r);
+            col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
